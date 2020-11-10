f77d7e6c41f25fc7d088c16b212d482bccc739fe
jrfeng <jrfeng331@qq.com>
2020-11-10 11:41:58 +0800

    fix compare bug & refactor 'PinyinComparator'

--- src/main/java/pinyin/util/PinyinComparator.java
+++ src/main/java/pinyin/util/PinyinComparator.java
@@ -1,6 +1,9 @@
 package pinyin.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import com.github.promeg.pinyinhelper.Pinyin;
 
@@ -10,43 +13,34 @@ public class PinyinComparator implements Comparator<String> {
         String s1 = getPinyinFirstLetter(o1).toLowerCase();
         String s2 = getPinyinFirstLetter(o2).toLowerCase();
 
-        IndexWrapper index1 = new IndexWrapper(0);
-        IndexWrapper index2 = new IndexWrapper(0);
+        List<Element> s1ElementList = asElementList(s1);
+        List<Element> s2ElementList = asElementList(s2);
 
-        while (index1.get() < s1.length() && index2.get() < s2.length()) {
-            char ch1 = s1.charAt(index1.get());
-            char ch2 = s2.charAt(index2.get());
+        return compare(s1ElementList, s2ElementList);
+    }
 
-            int result = ch1 - ch2;
-            if (isNumber(ch1) && isNumber(ch2)) {
-                result = getNumber(s1, index1) - getNumber(s2, index2);
-            }
+    private int compare(List<Element> listA, List<Element> listB) {
+        if (listA.isEmpty() && listB.isEmpty()) {
+            return 0;
+        }
 
+        if (listA.isEmpty()) {
+            return -1;
+        }
+
+        if (listB.isEmpty()) {
+            return 1;
+        }
+
+        int size = Math.min(listA.size(), listB.size());
+        for (int i = 0; i < size; i++) {
+            int result = listA.get(i).compareTo(listB.get(i));
             if (result != 0) {
                 return result;
             }
-
-            index1.inc();
-            index2.inc();
         }
 
         return 0;
-    }
-
-    private Integer getNumber(String s, IndexWrapper index) {
-        int end = index.get() + 1;
-
-        for (int i = index.get(); i < s.length(); i++) {
-            if (isNumber(s.charAt(i))) {
-                end = i;
-            }
-        }
-
-        Integer result = Integer.valueOf(s.substring(index.get(), end + 1));
-
-        index.set(end);
-
-        return result;
     }
 
     private static boolean isNumber(char c) {
@@ -111,23 +105,109 @@ public class PinyinComparator implements Comparator<String> {
         return firstLetter.toString();
     }
 
-    private static class IndexWrapper {
-        private int mIndex;
-
-        IndexWrapper(int initValue) {
-            mIndex = initValue;
+    private List<Element> asElementList(String string) {
+        if (string.isEmpty()) {
+            return Collections.emptyList();
         }
 
-        int get() {
-            return mIndex;
+        List<Element> elementList = new ArrayList<>();
+
+        StringBuilder stringBuffer = new StringBuilder();
+        StringBuilder intBuffer = new StringBuilder();
+
+        char c = string.charAt(0);
+        int lastType = Element.getType(c);
+
+        appendToBuffer(c, lastType, stringBuffer, intBuffer);
+
+        for (int i = 1; i < string.length(); i++) {
+            c = string.charAt(i);
+            int type = Element.getType(c);
+
+            if (type != lastType) {
+                elementList.add(asElement(lastType, stringBuffer, intBuffer));
+            }
+
+            lastType = type;
+            appendToBuffer(c, lastType, stringBuffer, intBuffer);
         }
 
-        void set(int index) {
-            mIndex = index;
+        elementList.add(asElement(lastType, stringBuffer, intBuffer));
+
+        return elementList;
+    }
+
+    private Element asElement(int type, StringBuilder stringBuffer, StringBuilder intBuffer) {
+        if (type == Element.TYPE_STRING) {
+            Element element = new Element(stringBuffer.toString());
+            clear(stringBuffer);
+            return element;
         }
 
-        void inc() {
-            mIndex += 1;
+        Element element = new Element(Integer.parseInt(intBuffer.toString()));
+        clear(intBuffer);
+        return element;
+    }
+
+    private void appendToBuffer(char c, int type, StringBuilder stringBuffer, StringBuilder intBuffer) {
+        if (type == Element.TYPE_STRING) {
+            stringBuffer.append(c);
+        } else {
+            intBuffer.append(c);
+        }
+    }
+
+    private static class Element implements Comparable<Element> {
+        public static final int TYPE_STRING = 0;
+        public static final int TYPE_INTEGER = 1;
+
+        int type;
+        String stringValue;
+        int intValue;
+
+        Element(String value) {
+            this.type = TYPE_STRING;
+            stringValue = value;
+        }
+
+        Element(int value) {
+            this.type = TYPE_INTEGER;
+            intValue = value;
+        }
+
+        @Override
+        public int compareTo(Element o) {
+            if (o.type != this.type) {
+                return compareType(o);
+            }
+
+            if (this.type == TYPE_STRING) {
+                return this.stringValue.compareToIgnoreCase(o.stringValue);
+            }
+
+            return this.intValue - o.intValue;
+        }
+
+        // 数字类型小于字符串类型
+        private int compareType(Element o) {
+            if (o.type == TYPE_INTEGER) {
+                return -1;
+            }
+
+            return 1;
+        }
+
+        public static int getType(char c) {
+            return isNumber(c) ? Element.TYPE_INTEGER : Element.TYPE_STRING;
+        }
+
+        @Override
+        public String toString() {
+            return "Element{" +
+                    "type=" + type +
+                    ", stringValue='" + stringValue + '\'' +
+                    ", intValue=" + intValue +
+                    '}';
         }
     }
 }
