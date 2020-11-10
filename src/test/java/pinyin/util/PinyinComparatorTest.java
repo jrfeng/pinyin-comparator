480743a545374e77629dd7f40b47e5926ace735d
jrfeng <jrfeng331@qq.com>
2020-11-10 11:42:21 +0800

    update 'PinyinComparatorTest'

--- src/test/java/pinyin/util/PinyinComparatorTest.java
+++ src/test/java/pinyin/util/PinyinComparatorTest.java
@@ -22,32 +22,82 @@ public class PinyinComparatorTest {
     }
 
     @Test
-    public void compare() {
+    public void compareTest1() {
         final String s1 = "单元测试";
         final String s2 = "单元测试";
 
-        final String s3 = "1单元测试";
-        final String s4 = "12单元测试";
+        final PinyinComparator comparator = new PinyinComparator();
+        assertEquals(0, comparator.compare(s1, s2));
+    }
 
-        final String s5 = "单元测试1";
-        final String s6 = "单元测试12";
-
-        final String s7 = "单元1测试";
-        final String s8 = "单元12测试";
-
-        final String s9 = "单元1测试";
-        final String s10 = "单元001测试";
-
-        final String s11 = "兵临城下";  // 首字母：BLCX
-        final String s12 = "海阔天空";  // 首字母：HKTK
+    @Test
+    public void compareTest2() {
+        final String s1 = "1单元测试";
+        final String s2 = "12单元测试";
 
         final PinyinComparator comparator = new PinyinComparator();
+        assertTrue(comparator.compare(s1, s2) < 0);
+    }
 
+    @Test
+    public void compareTest3() {
+        final String s1 = "单元测试1";
+        final String s2 = "单元测试12";
+
+        final PinyinComparator comparator = new PinyinComparator();
+        assertTrue(comparator.compare(s1, s2) < 0);
+    }
+
+    @Test
+    public void compareTest4() {
+        final String s1 = "单元1测试";
+        final String s2 = "单元12测试";
+
+        final PinyinComparator comparator = new PinyinComparator();
+        assertTrue(comparator.compare(s1, s2) < 0);
+    }
+
+    @Test
+    public void compareTest5() {
+        final String s1 = "单元1测试";
+        final String s2 = "单元001测试";
+
+        final PinyinComparator comparator = new PinyinComparator();
         assertEquals(0, comparator.compare(s1, s2));
-        assertTrue(comparator.compare(s3, s4) < 0);
-        assertTrue(comparator.compare(s5, s6) < 0);
-        assertTrue(comparator.compare(s7, s8) < 0);
-        assertEquals(0, comparator.compare(s9, s10));
-        assertTrue(comparator.compare(s11, s12) < 0);
+    }
+
+    @Test
+    public void compareTest6() {
+        final String s1 = "兵临城下";  // 首字母：BLCX
+        final String s2 = "海阔天空";  // 首字母：HKTK
+
+        final PinyinComparator comparator = new PinyinComparator();
+        assertTrue(comparator.compare(s1, s2) < 0);
+    }
+
+    @Test
+    public void compareTest7() {
+        final String s1 = "";
+        final String s2 = "";
+
+        final String s3 = "";
+        final String s4 = "你好hello";
+
+        final String s5 = "你好hello";
+        final String s6 = "";
+
+        final PinyinComparator comparator = new PinyinComparator();
+        assertEquals(0, comparator.compare(s1, s2));
+        assertEquals(-1, comparator.compare(s3, s4));
+        assertEquals(1, comparator.compare(s5, s6));
+    }
+
+    @Test
+    public void compareTest8() {
+        final String s1 = "hello123world";
+        final String s2 = "helloworld";
+
+        final PinyinComparator comparator = new PinyinComparator();
+        assertTrue(comparator.compare(s1, s2) < 0);
     }
 }
